0dc5d0f6b597dc568632c7c3c2f54f7adb6b227b
DESKTOP-NLHHV5B\quoc1 <quoc150620@gmail.com>
2023-04-04 17:02:23 +0700

    update commit

--- AutoUpdate/AutoUpdate/Form1.Designer.cs
+++ AutoUpdate/AutoUpdate/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace AutoUpdate
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,21 @@ namespace AutoUpdate
             this.label2.TabIndex = 1;
             this.label2.Text = "Version 1 OK";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Version 1 OK";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(317, 166);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -69,6 +80,7 @@ namespace AutoUpdate
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
