3ef9881d0bff6ed99e7013347788f66d2f8c374e
DESKTOP-NLHHV5B\quoc1 <quoc150620@gmail.com>
2023-04-04 16:29:32 +0700

    gg

--- AutoUpdate/AutoUpdate/Form1.Designer.cs
+++ AutoUpdate/AutoUpdate/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace AutoUpdate
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Version 2";
+            this.label1.Text = "Version 1";
             // 
             // Form1
             // 
